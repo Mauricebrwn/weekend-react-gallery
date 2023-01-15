22be79871b68983812693ea27114cd9a7b82738d
Maurice Brown <mauricebrwn6890@gmail.com>
2023-01-15 14:00:44 -0600

    appjsx imports gallery

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,34 @@
+import { useState, useEffect } from 'react';
 import React from 'react';
 import './App.css';
 import GalleryList from '../GalleryList/GalleryList';
+import axios from 'axios';
 
 function App() {
+
+  const[galleryData, setGalleryData] = useState([]);
+
+  useEffect (() => {
+    getGalleryList();
+  },[]);
+
+  const getGalleryList = () => {
+    axios
+    .get('gallery')
+    .then((response) => {
+      setGalleryData(response.data);
+    })
+    .catch((error) => {
+      console.log(error, 'something broke in axios.get')
+    });
+  };
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <GalleryList galleryData={galleryData}/>
       </div>
     );
 }

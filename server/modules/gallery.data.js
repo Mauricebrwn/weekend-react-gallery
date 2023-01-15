e3ab7bada3a13bd1eb37f318735b1433f89ad0b2
Maurice Brown <mauricebrwn6890@gmail.com>
2023-01-15 10:23:38 -0600

    added images

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/bella.jpeg', description: 'Photo of Bella at Forrest Hills', likes: 0 },
+    { id: 3, path: 'images/elli.jpeg', description: 'Photo of Elli at Forrest Hills', likes: 0 },
+    { id: 4, path: 'images/heather-maurice.jpeg', description: 'Photo of Heather and Maurice at Carlsbad', likes: 0 },
+    { id: 5, path: 'images/remy.jpeg', description: 'Photo of Remy at Target', likes: 0 },
+    { id: 6, path: 'images/tristan.jpeg', description: 'Photo of Tristan at Carlsbad', likes: 0 },
 ];
 
 module.exports = galleryItems;
